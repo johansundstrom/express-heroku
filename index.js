ff157203803c6da3e0bfad89ec63e524597fbc45
johansundstrom <johan.sundstrom@mdh.se>
2019-12-13 18:18:07 +0100

    provar include

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
+'use strict';
+
 let express = require('express');
 let port = process.env.PORT || 3000;
 let app = express();
+let ejs = require('ejs');
 
-app.use(express.static(__dirname + '/view'))
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/views'))
 app.use(express.static(__dirname + '/public/img'))
 app.use(express.static(__dirname + '/public/css'))
 
+
 app.get('/', function (req, res) {
     res.sendFile('index.html');
+    //res.render('index.ejs');
 });
 
 app.listen(port, function () {
